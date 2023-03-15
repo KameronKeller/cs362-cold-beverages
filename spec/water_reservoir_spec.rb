40fee51a5cb44e11d762ff6506b3932d4907c2ed
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 07:49:28 -0700

    Test attributes

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,10 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+	it 'has a capacity and initial water volume' do
+		reservoir = WaterReservoir.new()
+		expect(reservoir.capacity).to eq 10
+		expect(reservoir.current_water_volume).to eq 0
+	end
+
 end
