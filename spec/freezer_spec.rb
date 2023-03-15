d81342cc1ba23a4a4318ee36562e4ec098604aed
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 08:26:43 -0700

    Refactor to use before each, and test power on

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -2,10 +2,20 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
 
-	it 'has a capacity, and temperature' do
-		freezer = Freezer.new()
-		expect(freezer.capacity).to eq 100
-		expect(freezer.temperature).to eq 70
+	before :each do
+		@freezer = Freezer.new()
 	end
+
+	it 'has a capacity, temperature, and power setting' do
+		expect(@freezer.capacity).to eq 100
+		expect(@freezer.temperature).to eq 70
+		expect(@freezer.power).to eq :off
+	end
+
+	it 'can turn power on' do
+		@freezer.turn_on
+		expect(@freezer.power).to eq :on
+	end
+
 
 end
