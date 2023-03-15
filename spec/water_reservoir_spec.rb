c98c07e8fcfba5688dd71cf767f60951a27414d8
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 14:06:15 -0700

    Create water reservoir before each block

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,27 +2,28 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+	before :each do
+		@reservoir = WaterReservoir.new(10, 0)
+	end
+
 	it 'has a capacity and initial water volume' do
-		reservoir = WaterReservoir.new()
-		expect(reservoir.capacity).to eq 10
-		expect(reservoir.current_water_volume).to eq 0
+		expect(@reservoir.capacity).to eq 10
+		expect(@reservoir.current_water_volume).to eq 0
 	end
 
 	it 'is empty when current_water_volume is 0' do
-		reservoir = WaterReservoir.new(0, 0)
-		expect(reservoir).to be_empty
+		expect(@reservoir).to be_empty
 	end
 
 	it 'can fill the current_water_volume to capacity' do
-		reservoir = WaterReservoir.new(10, 0)
-		reservoir.fill
-		expect(reservoir.current_water_volume).to eq 10
+		@reservoir.fill
+		expect(@reservoir.current_water_volume).to eq 10
 	end
 
 	it 'can drain a given volume' do
-		reservoir = WaterReservoir.new(10, 10)
-		reservoir.drain(5)
-		expect(reservoir.current_water_volume).to eq 5
+		@reservoir.current_water_volume = 10
+		@reservoir.drain(5)
+		expect(@reservoir.current_water_volume).to eq 5
 	end
 
 end
