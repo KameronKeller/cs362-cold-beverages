0957a2aec87ae169f18abaed47d11c7aa381e770
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 09:12:17 -0700

    Test total capacity method

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -4,8 +4,8 @@ describe 'A refrigerator' do
 
 	before :each do
 		@item = Item.new('FAKE', 10)
-		@chiller = Chiller.new
-		@freezer = Freezer.new
+		@chiller = Chiller.new(100)
+		@freezer = Freezer.new(100)
 		@water_reservoir = WaterReservoir.new
 		@water_dispenser = WaterDispenser.new(@water_reservoir)
 		@refrigerator = Refrigerator.new(@chiller, @freezer, @water_dispenser, @water_reservoir)
@@ -21,4 +21,7 @@ describe 'A refrigerator' do
 		expect(@refrigerator.freezer.contents).to include(@item)
 	end
 
+	it 'can return the total capacity' do
+		expect(@refrigerator.total_capacity).to eq 200
+	end
 end
