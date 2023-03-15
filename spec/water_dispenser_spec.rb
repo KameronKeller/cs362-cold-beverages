5cdaf2d6aa9d7a3551b9cb1a7c2040cf54ea3e3a
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 14:03:56 -0700

    Create water dispenser before each block

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -3,34 +3,28 @@ require_relative '../lib/water_dispenser'
 describe 'A water dispenser' do
 
 	before :each do
-
+		@reservoir = WaterReservoir.new(10, 10)
+		@dispenser = WaterDispenser.new(@reservoir)
 	end
 
 	it 'has a reservoir' do
-		reservoir = WaterReservoir.new()
-		dispenser = WaterDispenser.new(reservoir)
-		expect(dispenser.reservoir).to be_an_instance_of WaterReservoir
+		expect(@dispenser.reservoir).to be_an_instance_of WaterReservoir
 	end
 
 	it 'will only despense up to the available amount of water in the reservoir' do
-		reservoir = WaterReservoir.new(10, 0)
-		dispenser = WaterDispenser.new(reservoir)
+		@reservoir.current_water_volume = 0
 		vessel = Vessel.new('TestVessel', 5)
-		expect(dispenser.dispense(vessel)).to eq 0
+		expect(@dispenser.dispense(vessel)).to eq 0
 	end
 
 	it 'will fill a vessel with dispense if the reservoir has enough water' do
-		reservoir = WaterReservoir.new(10, 10)
-		dispenser = WaterDispenser.new(reservoir)
 		vessel = Vessel.new('TestVessel', 5)
-		expect(dispenser.dispense(vessel)).to eq 5
+		expect(@dispenser.dispense(vessel)).to eq 5
 	end
 
 	it 'a vessel is filled when it is dispensed into' do
-		reservoir = WaterReservoir.new(10, 10)
-		dispenser = WaterDispenser.new(reservoir)
 		vessel = Vessel.new('TestVessel', 5)
-		dispenser.dispense(vessel)
+		@dispenser.dispense(vessel)
 		expect(vessel).to_not be_empty
 	end
 
