9b64c167e389547ca499f00dee604bd03ec95261
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 07:47:41 -0700

    Test dispense method

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,13 +2,22 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+	before :each do
+
+	end
+
 	it 'has a reservoir' do
 		reservoir = WaterReservoir.new()
 		dispenser = WaterDispenser.new(reservoir)
 		expect(dispenser.reservoir).to be_an_instance_of WaterReservoir
 	end
 
-
+	it 'can dispense into a vessel' do
+		reservoir = WaterReservoir.new(10, 10)
+		dispenser = WaterDispenser.new(reservoir)
+		vessel = Vessel.new('TestVessel', 5)
+		expect(dispenser.dispense(vessel)).to eq 5
+	end
 
 
 end
