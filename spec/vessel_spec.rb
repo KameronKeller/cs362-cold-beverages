b7737829f9d88c0cfc4612540a0856f3095c5a01
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 14:00:11 -0700

    Create vessels before each block

--- spec/vessel_spec.rb
+++ spec/vessel_spec.rb
@@ -2,20 +2,21 @@ require_relative '../lib/vessel'
 
 describe 'A vessel for holding liquid' do
 
+  before :each do
+    @vessel = Vessel.new('FAKE', 100)
+  end
+
   it 'has a name and volume' do
-    vessel = Vessel.new
-    expect(vessel.name).to eq('FAKE')
-    expect(vessel.volume).to eq(100)
+    expect(@vessel.name).to eq('FAKE')
+    expect(@vessel.volume).to eq(100)
   end
 
   it 'is initially empty' do
-    vessel = Vessel.new('FAKE', 100)
-    expect(vessel).to be_empty
+    expect(@vessel).to be_empty
   end
 
   it 'is no longer empty when we fill it' do
-    vessel = Vessel.new('FAKE', 100)
-    vessel.fill
-    expect(vessel).to_not be_empty
+    @vessel.fill
+    expect(@vessel).to_not be_empty
   end
 end
