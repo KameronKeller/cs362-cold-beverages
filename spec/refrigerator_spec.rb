1391d65806ac18ffe7f0eb76cb59de673f106efc
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 09:08:41 -0700

    Test chill method

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -2,4 +2,22 @@ require_relative '../lib/refrigerator'
 
 describe 'A refrigerator' do
 
+	before :each do
+		@item = Item.new('FAKE', 10)
+		@chiller = Chiller.new
+		@freezer = Freezer.new
+		@water_reservoir = WaterReservoir.new
+		@water_dispenser = WaterDispenser.new(@water_reservoir)
+		@refrigerator = Refrigerator.new(@chiller, @freezer, @water_dispenser, @water_reservoir)
+	end
+
+	it 'can chill an item' do
+		@refrigerator.chill(@item)
+		expect(@refrigerator.chiller.contents).to include(@item)
+	end
+
+	it 'can freeze an item' do
+
+	end
+
 end
