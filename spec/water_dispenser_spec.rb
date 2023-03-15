6ce00bf063faa956f3cea604f482bc3b9deb14d9
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-15 07:42:13 -0700

    Test dispenser attributs

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,13 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+	it 'has a reservoir' do
+		reservoir = WaterReservoir.new()
+		dispenser = WaterDispenser.new(reservoir)
+		expect(dispenser.reservoir).to be_an_instance_of WaterReservoir
+	end
+
+
+
+
 end
